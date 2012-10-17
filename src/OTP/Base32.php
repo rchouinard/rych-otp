63315edbe8d7a6c85e09b44d111e9f312f291344
Ryan Chouinard <rchouinard@gmail.com>
2012-10-17 16:59:00 -0400

    Add basic base32 implementation
    This could be done a lot better, but I decided to take some shortcuts so
    I can get some traction on the project. Should probably return to this
    and refactor later.

---
+++ src/OTP/Base32.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace OTP;
+
+class Base32
+{
+
+    private $charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+    public function decode($data)
+    {
+        $decoded = '';
+
+        if ($data) {
+            // 'ifba====' => 'IFBA'
+            $data = rtrim(strtoupper($data), '=');
+
+            if (!preg_match('/^[A-Z2-7]+$/', $data)) {
+                throw new \InvalidArgumentException('Invalid base32 string');
+            }
+
+            $binString = '';
+            // 'IFBA' => 01000 00101 00001 00000
+            foreach (str_split($data) as $char) {
+                $binString .= str_pad(decbin(strpos($this->charset, $char)), 5, 0, STR_PAD_LEFT);
+            }
+
+            // 01000 00101 00001 00000 => 01000001 01000010
+            // Assuming it's safe to drop the trailing bits, as if this is a
+            // valid Base32 string, they'll be padding zeros anyway.
+            $binString = substr($binString, 0, (floor(strlen($binString) / 8) * 8));
+
+            // 01000001 01000010 => 'AB'
+            for ($offset = 0; $offset < strlen($binString); $offset += 8) {
+                $chunk = str_pad(substr($binString, $offset, 8), 8, 0, STR_PAD_RIGHT);
+                $decoded .= trim(chr(bindec($chunk)));
+            }
+        }
+
+        return $decoded;
+    }
+
+    public function encode($data)
+    {
+        $encoded = '';
+
+        if ($data) {
+            $binString = '';
+            // 'AB' => 01000001 01000010
+            foreach (str_split($data) as $char) {
+                $binString .= str_pad(decbin(ord($char)), 8, 0, STR_PAD_LEFT);
+            }
+
+            // 01000001 01000010 => 01000 00101 00001 00000 => 'IFBA'
+            for ($offset = 0; $offset < strlen($binString); $offset += 5) {
+                $chunk = str_pad(substr($binString, $offset, 5), 5, 0, STR_PAD_RIGHT);
+                $encoded .= $this->charset[bindec($chunk)];
+            }
+
+            // 'IFBA' => 'IFBA===='
+            if (strlen($encoded) % 8) {
+                $encoded .= str_repeat('=', 8 - (strlen($encoded) % 8));
+            }
+        }
+
+        return $encoded;
+    }
+
+}
