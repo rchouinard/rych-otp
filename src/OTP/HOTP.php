454ce08efd33796263021b0904c45ccc03a7399e
Ryan Chouinard <rchouinard@gmail.com>
2012-10-18 00:07:28 -0400

    Add method to verify an OTP, and add method documentation.

--- src/OTP/HOTP.php
+++ src/OTP/HOTP.php
@@ -9,30 +9,53 @@ require_once 'OTP/Base32.php';
 class HOTP
 {
 
+    /**
+     * @var Base32
+     */
+    private $base32;
+
+    /**
+     * @var string
+     */
     private $seed;
 
     /**
-     * Generate a base32 encoded seed value
+     * Class constructor.
      *
-     * This method will also set the seed value for future operation to the
-     * generated value.
-     *
-     * @param integer $bytes Number of bytes to use. Default of 10 produces an
-     *     80-bit seed value.
-     * @return string The base32 encoded seed value
+     * @return void
      */
-    public function generateSeed($bytes = 10)
+    public function __construct()
     {
-        $count = (int) $bytes;
-        $output = openssl_random_pseudo_bytes($count);
-
-        $b32 = new Base32;
-        return $this->seed = $b32->encode($output);
+        $this->base32 = new Base32;
     }
 
     /**
+     * Generate a base32 encoded seed value.
+     *
+     * This method should only be called on the first-time setup of a new client
+     * token, or if the client token must be reseeded. Subsequent use of this
+     * class for a given client should use the setSeed() method to provide the
+     * previously generated stored seed value.
+     *
+     * Will also set the seed value for future operation to the generated value.
+     *
+     * @param integer $bytes Number of bytes to use. Default of 20 produces an
+     *     160-bit seed value as recommended by RFC 4226 Section 4 R6.
+     * @return string The base32 encoded seed value.
+     */
+    public function generateSeed($bytes = 20)
+    {
+        $bytes = (int) $bytes;
+        $output = $this->genRandomBytes($bytes);
+
+        return $this->seed = $this->base32->encode($output);
+    }
+
+    /**
+     * Set the seed value to use for future operations.
+     *
      * @param string $seed The base32 encoded seed value
-     * @return self
+     * @return OTP\HOTP Returns an instance of self for method chaining.
      */
     public function setSeed($seed)
     {
@@ -41,7 +64,9 @@ class HOTP
     }
 
     /**
-     * @return string The base32 encoded seed value
+     * Get the current seed value.
+     *
+     * @return string The base32 encoded seed value.
      */
     public function getSeed()
     {
@@ -49,24 +74,51 @@ class HOTP
     }
 
     /**
-     * Generate a one-time password based on the current seed and given counter
+     * Generate an OTP based on the current seed and given counter.
      *
-     * @param string $counter
-     * @return integer
+     * @param integer $counter The counter value to use for the OTP.
+     * @param integer $length Desired length of the OTP. Defaults to 6.
+     * @return integer Returns the generated OTP.
      */
     public function generateOTP($counter, $length = 6)
     {
-        $b32 = new Base32;
+        // Length must be at least 6, according to RFC 4226 Section 4 R4.
+        $length = max((int) $length, 6);
 
-        // Counter must be a 64-bit integer
+        // Counter must be a 64-bit integer, so we fake it.
         $counter = pack('N*', 0) . pack('N*', $counter);
-        $hash = hash_hmac('sha1', $counter, $b32->decode($this->getSeed()), true);
+        $hash = hash_hmac('sha1', $counter, $this->base32->decode($this->getSeed()), true);
 
         return $this->truncate($hash) % pow(10, $length);
     }
 
     /**
-     * Extract 4 bytes from the hash value, used to calculate the OTP digits
+     * Verify an OTP using the specified counter and an optional window.
+     *
+     * @param integer $otp The user-supplied OTP value.
+     * @param integer $counter The counter value to base the OTP on.
+     * @param integer $window How many times the counter should be adjusted for
+     *     error. Defaults to 4.
+     * @return integer Returns the counter value which matched on success, or
+     *     false on failure.
+     */
+    public function verifyOTP($otp, $counter, $window = 4)
+    {
+        $valid = false;
+        $counter = (int) $counter;
+
+        for ($current = $counter; $current <= $counter + (int) $window; ++$current) {
+            if ($otp == $this->generateOTP($current, strlen($otp))) {
+                $valid = $current;
+                break;
+            }
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Extract 4 bytes from the hash value, used to calculate the OTP digits.
      *
      * Uses the method defined in RFC 4226, Section 5.3.
      *
@@ -84,6 +136,65 @@ class HOTP
         $value |= (ord($hash[$offset + 3]) & 0xff);
 
         return $value;
+    }
+
+    /**
+     * Generate a random byte string of the requested length.
+     *
+     * @param integer $count Number of bytes to generate.
+     * @return string Random byte string.
+     */
+    private function genRandomBytes($count)
+    {
+        $count = (int) $count;
+
+        // Try OpenSSL's random generator
+        $output = '';
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $strongCrypto = false;
+            // NOTE: The $strongCrypto argument here isn't telling OpenSSL to
+            // generate (or not) cryptographically secure data. It's passed
+            // by reference, and will be set to true or false after the
+            // function call to indicate whether or not OpenSSL is confident
+            // that the generated data can be used for cryptographic operations.
+            $output = openssl_random_pseudo_bytes($count, $strongCrypto);
+            if ($strongCrypto && strlen($output) == $count) {
+                return $output;
+            }
+        }
+
+        // Try creating an mcrypt IV
+        $output = '';
+        if (function_exists('mcrypt_create_iv')) {
+            $output = mcrypt_create_iv($count, MCRYPT_DEV_URANDOM);
+            if (strlen($output) == $count) {
+                return $output;
+            }
+        }
+
+        // Try reading from /dev/urandom, if present
+        $output = '';
+        if (is_readable('/dev/urandom') && ($fh = fopen('/dev/urandom', 'rb'))) {
+            $output = fread($fh, $count);
+            fclose($fh);
+            if (strlen($output) == $count) {
+                return $output;
+            }
+        }
+
+        // Fall back to a locally generated "random" string as last resort
+        $randomState = microtime();
+        if (function_exists('getmypid')) {
+            $randomState .= getmypid();
+        }
+        $output = '';
+        for ($i = 0; $i < $count; $i += 16) {
+            $randomState = md5(microtime() . $randomState);
+            $output .= md5($randomState, true);
+        }
+        $output = substr($output, 0, $count);
+
+        return $output;
     }
 
 }
--- tests/OTP/Test/HOTPTest.php
+++ tests/OTP/Test/HOTPTest.php
@@ -11,13 +11,60 @@ class HOTPTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function testVectors()
+    public function testGenerateSeed()
     {
         $hotp = new HOTP;
-        $hotp->setSeed('22K3UFWSQLCDGNXH');
-        // BO5IESBLQNUVLFUB
 
+        $seed = $hotp->generateSeed(10);
+        $this->assertRegExp('/^[A-Z2-7]{16}$/', $seed);
+        $this->assertEquals($seed, $hotp->getSeed());
+
+        $seed = $hotp->generateSeed(20);
+        $this->assertRegExp('/^[A-Z2-7]{32}$/', $seed);
+        $this->assertEquals($seed, $hotp->getSeed());
+    }
+
+    /**
+     * @test
+     */
+    public function testGenerateOTP()
+    {
+        $hotp = new HOTP;
+
+        $hotp->setSeed('22K3UFWSQLCDGNXH');
         $this->assertEquals('466474', $hotp->generateOTP(45016826));
+        $this->assertEquals('451890', $hotp->generateOTP('1'));
+        $this->assertEquals('951917', $hotp->generateOTP('2'));
+        $this->assertEquals('42114359', $hotp->generateOTP('123456', 8));
+
+        $hotp->setSeed('GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ');
+        $this->assertEquals('616632', $hotp->generateOTP(45016826));
+        $this->assertEquals('287082', $hotp->generateOTP('1'));
+        $this->assertEquals('359152', $hotp->generateOTP('2'));
+        $this->assertEquals('96746508', $hotp->generateOTP('123456', 8));
+    }
+
+    /**
+     * @test
+     */
+    public function testVerifyOTP()
+    {
+        $hotp = new HOTP;
+
+        $hotp->setSeed('22K3UFWSQLCDGNXH');
+        // Test that look-ahead counter correction works as expected
+        // Counter values higher than expected, but still within the window,
+        // should work. Lower values should not, lest we find that old OTPs
+        // can be reused.
+        //
+        // Counter value = 100, OTP generated with 100
+        $this->assertTrue('100' == $hotp->verifyOTP('785234', '100'));
+
+        // Counter value = 98, OTP generated with 100
+        $this->assertTrue('100' == $hotp->verifyOTP('785234', '98', 4));
+
+        // Counter value = 101, OTP generated with 100
+        $this->assertFalse((bool) $hotp->verifyOTP('785234', '101', 4));
     }
 
 }
